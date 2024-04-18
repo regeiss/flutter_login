3fa60e3c2ca75398cc0ec02bd494481f81c830a5
regeiss <roberto.geiss.nh@gmail.com>
2024-04-18 15:03:03 -0300

    inicial

---
+++ lib/src/core/api/endpoints.dart
@@ -0,0 +1,6 @@
+class Endpoints 
+{
+  static String baseURL =
+      "https://run.mocky.io/v3/";
+  static String loginURL = "333d37fa-620b-4a5c-82f1-81331925a6e7";
+}
---
+++ lib/src/features/login/screen/login.dart
@@ -0,0 +1,108 @@
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+import '../../usuario/provider/user_controller.dart';
+
+class LoginScreen extends StatefulHookConsumerWidget {
+  const LoginScreen({super.key});
+
+  @override
+  ConsumerState<ConsumerStatefulWidget> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends ConsumerState<LoginScreen> 
+{
+    TextEditingController email = TextEditingController();
+    TextEditingController password = TextEditingController();
+    bool _isHidden = true;
+
+  @override
+  void initState() 
+  {
+    email.text = "miftahulinc@gmail.com"; //innitail value of text field
+    password.text = "huda12345";
+    super.initState();
+  }
+
+  void _togglePasswordView() {
+    setState(() {
+      _isHidden = !_isHidden;
+    });
+  }
+
+  void showSnackbar(BuildContext context, String text) {
+    final snackBar = SnackBar(
+      content: Text(text),
+      duration: const Duration(seconds: 5),
+    );
+    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Login - protótipo"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(30),
+        child: Column(
+          children: [
+            TextField(
+              controller: email,
+              decoration: const InputDecoration(
+                labelText: "Email",
+                icon: Icon(Icons.people), //icon at head of input
+              ),
+            ),
+            TextField(
+              controller: password,
+              obscureText: _isHidden,
+              decoration: InputDecoration(
+                icon: const Icon(Icons.lock), //icon at head of input
+                labelText: "Senha",
+                suffixIcon: IconButton(
+                  onPressed: _togglePasswordView,
+                  icon: Icon(
+                    _isHidden ? Icons.visibility : Icons.visibility_off,
+                  ),
+                ), //icon at tail of input
+              ),
+            ),
+            const SizedBox(height: 20),
+            Row(
+              children: [
+                Expanded(
+                  child: ElevatedButton(
+                    onPressed: () {
+                      ref
+                          .read(userControllerProvider.notifier)
+                          .login(
+                            email: email.text,
+                            password: password.text,
+                          )
+                          .then(
+                            (res) => {
+                              res.fold(
+                                (l) => {
+                                  showSnackbar(context, l),
+                                },
+                                (r) => {
+                                  Navigator.pushReplacementNamed(
+                                      context, 'Home'),
+                                },
+                              ),
+                            },
+                          );
+                    },
+                    child: const Text("Login"),
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
